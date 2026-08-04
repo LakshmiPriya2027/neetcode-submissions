32fb97368a2b5f95f9ba0df24d2c76b835f75a12
LAKSHMI PRIYA <lakshmipriyaappala@gmail.com>
2026-08-04 18:00:20 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='{'){
+                st.push('}');
+            }
+            else if(ch=='('){
+                st.push(')');
+            }
+            else if(ch=='['){
+                st.push(']');
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                if(ch!=st.pop()){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}

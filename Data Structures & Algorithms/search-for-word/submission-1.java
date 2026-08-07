145dc35ce8820e01e6777c20e6351d4599713f22
LAKSHMI PRIYA <lakshmipriyaappala@gmail.com>
2026-08-07 19:14:50 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int rows=board.length;
+        int columns=board[0].length;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                if(consistsword(board,word,i,j,0,rows,columns)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean consistsword(char[][] board,String word,int r,int c,int k,int rows,int columns){
+        if(k==word.length()){
+            return true;
+        }
+        if(r<0 || c<0 ||r>=rows || c>=columns ||board[r][c]!=word.charAt(k) || board[r][c]=='#'){
+            return false;
+        }
+        board[r][c]='#';
+      //  boolean result=consistsword(board,word,r+1,c,k+1,rows,columns)||consistsword(board,word,r-1,c,k+1,rows,columns)||consistsword(board,word,r,c+1,k+1,rows,columns)||consistsword(board,word,r,c-1,k+1,rows,columns);
+        int[] dx={1,-1,0,0};
+        int[] dy={0,0,1,-1};
+        boolean result=false;
+        for(int d=0;d<4;d++){
+            result=result||consistsword(board,word,r+dx[d],c+dy[d],k+1,rows,columns);
+        }
+        board[r][c]=word.charAt(k);
+        return result;
+    }
+}
